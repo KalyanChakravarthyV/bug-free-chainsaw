52f3234d39db545341a162ac8f4ea2deaab6eb4a
Kalyan Chakravarthy V <KalyanChakravarthyV@gmail.com>
2025-03-23 22:23:16 +0530

    Clean up variables

--- main.tf
+++ main.tf
@@ -20,14 +20,10 @@ terraform {
   }
 }
 
-/*
-provider "aws" {
-  region  = var.AWS_REGION
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-}
-*/
-/*
-    Modules
+/* Modules
 */
 
+module "lambda"{
+    source = "./lambda"
+    count = 0
+}
--- variables.tf
+++
@@ -1,5 +0,0 @@
-variable "AWS_ACCESS_KEY_ID" {} 
-variable "AWS_REGION" {}
-variable "AWS_SECRET_ACCESS_KEY" {
-      sensitive = true
-}
